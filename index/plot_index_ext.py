32fd42c120327238886b8e78c719e1b39325664f
aelaouni <35902028+aelaouni@users.noreply.github.com>
2018-03-07 17:24:31 +0100

    Update plot_index_ext.py

--- index/plot_index_ext.py
+++ index/plot_index_ext.py
@@ -1,5 +1,10 @@
+import scipy.ndimage as ndimage
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+from matplotlib.colors import LinearSegmentedColormap
 
 import matplotlib.pyplot as plt
+import numpy as np
 import csv
 
 x = []
@@ -22,26 +27,52 @@ with open('ext.txt','r') as csvfile:
         yy.append(float(row[1]))
 
 
-# plt.plot(x,y, label='upwelling index')
-# plt.xlabel('latitudes')
-# plt.ylabel('intensity')
-# plt.title('Spatial variation of upwelling index')
-# plt.legend()
-# plt.show()
+
+maxss=np.amax(yy, axis=0) ;
+lenn=len(xx)
+
+print(maxss)
+print(lenn)
+
+Matrix = [[0 for i in range(lenn)] for j in range(int(maxss))] 
+
+for j in range(lenn):
+     for i in range(int(maxss)):
+        Matrix[i][j]=float('nan')
 
 
-fig, ax1 = plt.subplots()
 
-ax1.plot(x, y, 'b-')
-ax1.set_xlabel('latitudes')
-# Make the y-axis label, ticks and tick labels match the line color.
-ax1.set_ylabel('Upwelling intensity', color='b')
-ax1.tick_params('y', colors='b')
 
-ax2 = ax1.twinx()
-ax2.plot(xx, yy, 'r-')
-ax2.set_ylabel('Upwelling extension (km)', color='r')
-ax2.tick_params('y', colors='r')
+Matrix = [[0 for i in range(lenn)] for j in range(int(maxss))] 
 
-fig.tight_layout()
+for j in range(lenn):
+     for i in range(int(yy[j])):
+        Matrix[i][j]=y[j]
+
+
+levels = [0.1,2.5,4]
+#plt.contour(img, levels, colors='k')
+plt.contourf(Matrix, 100,cmap = LinearSegmentedColormap.from_list('mycmap', ['white', 'blue', 'yellow', 'red', 'black']))
+#plt.contourf(img, 100)
+#plt.contour(img, colors='black', linewidth=.5)
+#plt.clim(1,5.8)
+
+cb=plt.colorbar(orientation="horizontal")
+tick_locator = ticker.MaxNLocator(nbins=5)
+cb.locator = tick_locator
+cb.update_ticks()
+cb.set_label("Upwelling intensity ($^{\circ}$C)")
+#labels = ['J', 'F', 'M', 'A','M','J','J','A','S','O','N','D']
+labelsy = ['$36N^{\circ}$', '$33^{\circ}N$', '$30^{\circ}N$', '$27^{\circ}N$','$24^{\circ}N$','$21^{\circ}N$']
+#xxx = np.arange(0,46,3.835)
+yyy = np.arange(0,375,64)
+
+
+plt.xticks(yyy, labelsy, rotation='horizontal')
+plt.ylabel('Spatial extension (km)')
+plt.xlabel('Latitudes')
+
+plt.axhline(0, color='white')
+plt.axvline(0, color='white')
+
 plt.show()
